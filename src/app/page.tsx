c42a26af26d57ebaf747111675663bad84d92d2c
Kasperi Jaakkola <kassuvaa@gmail.com>
2025-02-01 20:58:20 +0200

    Fixed autodeployment test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
         <h1>UNDER CONSTRUCTION</h1>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>webpage coming soon...</p>
       </footer>
     </div>
   );
