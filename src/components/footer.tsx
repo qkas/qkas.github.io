7cbcb19a1ece75760d0dfc3d9d93cd8c83967f36
qkas <kassuvaa@gmail.com>
2025-02-12 15:04:42 +0200

    Tabs open on new tab and improved formatting

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { Copyright } from "@mui/icons-material";
 
@@ -6,9 +6,16 @@ export default function Footer() {
   return (
     <footer className="flex items-center font-[family-name:var(--font-geist-mono)] mt-20 p-8">
       <Copyright fontSize="inherit" className="mr-1" />
-      <span>2025, Kasperi Jaakkola.
-        Made with <Link href="https://nextjs.org/" className="hover:underline">Next.js</Link>
+      <span>
+        2025,{" "}
+        <Link href="https://www.linkedin.com/in/kasperi-jaakkola/" className="hover:underline hover:uppercase" rel="noopener noreferrer" target="_blank">
+          Kasperi Jaakkola
+        </Link>
+        . Made with{" "}
+        <Link href="https://nextjs.org/" className="hover:underline hover:uppercase" rel="noopener noreferrer" target="_blank">
+          Next.js
+        </Link>
       </span>
     </footer>
-  )
+  );
 }
