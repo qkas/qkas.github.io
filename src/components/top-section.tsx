fc62eb855ae15e3606a22537f84fa29450cd5192
qkas <kassuvaa@gmail.com>
2025-02-13 19:32:34 +0200

    Added links to linkedin to picture and name

--- src/components/top-section.tsx
+++ src/components/top-section.tsx
@@ -5,16 +5,25 @@ export default function TopSection() {
   return (
     <main className="md:mt-10 font-[family-name:var(--font-geist-mono)]">
       <div className="flex">
+      <Link
+          href="https://www.linkedin.com/in/kasperi-jaakkola/" 
+          className="hover:scale-105 transition min-w-32"
+          rel="noopener noreferrer" target="_blank"
+      >
       <Image
         className="border-foreground border-2"
         src="/bear.png"
         alt="Profile"
         width={200}
         height={200}
-        />
+      />
+      </Link>
       <div className="sm:mx-10 mx-5">
-        <Link href="https://www.linkedin.com/in/kasperi-jaakkola/" className="font-bold text-4xl md:text-5xl lg:text-6xl hover:scale-105 hover:underline hover:uppercase transition">
-          Kasperi Jaakkola
+        <Link
+          href="https://www.linkedin.com/in/kasperi-jaakkola/" 
+          className="font-bold text-4xl md:text-5xl lg:text-6xl hover:scale-105 hover:underline hover:uppercase transition"
+          rel="noopener noreferrer" target="_blank"
+        > Kasperi Jaakkola
         </Link>
         <section className="text-sm md:text-base lg:text-lg mt-4 md:mt-6 flex-wrap max-w-xl">
           Hey, welcome to my profile. I'm a software developer from {" "}
@@ -22,8 +31,7 @@ export default function TopSection() {
             href="https://www.google.com/search?q=turku+finland"
             className="hover:underline hover:uppercase"
             rel="noopener noreferrer" target="_blank"
-            >
-            Turku, Finland
+          > Turku, Finland
           </Link>, passionate about creating games and applications for others to enjoy.
           This is my personal website where I'll be sharing my projects.
         </section>
