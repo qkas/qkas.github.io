4f1d7d21485f3d3829918dac4e0d02bd362b03ab
qkas <kassuvaa@gmail.com>
2025-02-13 18:09:13 +0200

    Made header item gap and font size responsible

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <nav className="flex mx-10 py-10 gap-10 font-bold font-[family-name:var(--font-geist-mono)] ">
-      <Link href="#profile" className="text-3xl hover:scale-105 hover:underline hover:uppercase transition">
+    <nav className="flex flex-wrap text-xl sm:text-2xl md:text-3xl mx-10 py-10 gap-4 sm:gap-6 md:gap-10 font-bold font-[family-name:var(--font-geist-mono)] ">
+      <Link href="#profile" className="hover:scale-105 hover:underline hover:uppercase transition">
         profile
       </Link>
-      <Link href="#about" className="text-3xl hover:scale-105 hover:underline hover:uppercase transition">
+      <Link href="#about" className="hover:scale-105 hover:underline hover:uppercase transition">
         about
       </Link>
-      <Link href="#skills" className="text-3xl hover:scale-105 hover:underline hover:uppercase transition">
+      <Link href="#skills" className="hover:scale-105 hover:underline hover:uppercase transition">
         skills
       </Link>
-      <Link href="#contact" className="text-3xl hover:scale-105 hover:underline hover:uppercase transition">
+      <Link href="#contact" className="hover:scale-105 hover:underline hover:uppercase transition">
         contact
       </Link>
     </nav>
