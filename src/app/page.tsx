f0876b973c83d27179526e8ec5c885791288f89f
qkas <kassuvaa@gmail.com>
2025-02-13 20:58:21 +0200

    Moved temp space into page.tsx and added wip info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <div>
       <Header/>
       <TopSection/>
+      <div className="my-20 font-[family-name:var(--font-geist-mono)]">WEBSITE IN PROGRESS, MORE CONTENT COMING SOON</div>
+      <div className="my-96">{/* temporary space for footer */}</div>
     </div>
   );
 }
--- src/components/top-section.tsx
+++ src/components/top-section.tsx
@@ -5,43 +5,38 @@ export default function TopSection() {
   return (
     <main className="md:mt-10 font-[family-name:var(--font-geist-mono)]">
       <div className="flex">
-      <Link
-          href="https://www.linkedin.com/in/kasperi-jaakkola/" 
+        <Link
+          href="https://www.linkedin.com/in/kasperi-jaakkola/"
           className="hover:scale-105 transition min-w-32"
           rel="noopener noreferrer" target="_blank"
-      >
-      <Image
-        className="border-foreground border-2"
-        src="/bear.png"
-        alt="Profile"
-        width={200}
-        height={200}
-      />
-      </Link>
-      <div className="sm:mx-10 mx-5">
-        <Link
-          href="https://www.linkedin.com/in/kasperi-jaakkola/" 
-          className="font-bold text-3xl md:text-5xl lg:text-6xl hover:scale-105 hover:underline hover:uppercase transition"
-          rel="noopener noreferrer" target="_blank"
-        > Kasperi Jaakkola
+        >
+          <Image
+            className="border-foreground border-2"
+            src="/bear.png"
+            alt="Profile"
+            width={200}
+            height={200}
+          />
         </Link>
-        <section className="text-sm md:text-base lg:text-lg mt-4 md:mt-6 flex-wrap max-w-xl">
-          Hey, welcome to my profile. I'm a software developer from {" "}
+        <div className="sm:mx-10 mx-5">
           <Link
-            href="https://www.google.com/search?q=turku+finland"
-            className="hover:underline hover:uppercase"
+            href="https://www.linkedin.com/in/kasperi-jaakkola/"
+            className="font-bold text-3xl md:text-5xl lg:text-6xl hover:scale-105 hover:underline hover:uppercase transition"
             rel="noopener noreferrer" target="_blank"
-          > Turku, Finland
-          </Link>, passionate about creating games and applications for others to enjoy.
-          This is my personal website where I'll be sharing my projects.
-        </section>
+          > Kasperi Jaakkola
+          </Link>
+          <section className="text-sm md:text-base lg:text-lg mt-4 md:mt-6 flex-wrap max-w-xl">
+            Hey, welcome to my profile. I'm a software developer from {" "}
+            <Link
+              href="https://www.google.com/search?q=turku+finland"
+              className="hover:underline hover:uppercase"
+              rel="noopener noreferrer" target="_blank"
+            > Turku, Finland
+            </Link>, passionate about creating games and applications for others to enjoy.
+            This is my personal website where I'll be sharing my projects.
+          </section>
+        </div>
       </div>
-      {/* <div className="ml-auto">
-        <h1 className="font-bold text-3xl">Level <span className="border-foreground text-xl font-light border-2 p-1 text-md rounded-full">10</span></h1>
-        <p className="mt-5">Still learning...</p>
-      </div> */}
-      </div>
-      <div className="my-96">{/* temporary space for footer */}</div>
     </main>
   )
 }
