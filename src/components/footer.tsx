a078cb5c4057e173ef1cb803b9deb2aa805e5a04
qkas <kassuvaa@gmail.com>
2025-02-14 15:18:19 +0200

    Aligned text to center on small screens

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,12 @@ import { Copyright } from "@mui/icons-material";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center text-xs sm:text-sm md:text-base font-[family-name:var(--font-geist-mono)] mt-20 px-10 py-8">
-      <Copyright fontSize="inherit" className="mr-1" />
+    <footer className="flex justify-center text-center items-center text-xs sm:text-sm md:text-base font-[family-name:var(--font-geist-mono)] mt-20 px-10 py-8">
       <span>
-        2025,{" "}
+        <span className="justify-start">
+          <Copyright fontSize="inherit" className="mr-1" />
+          2025,{" "}
+        </span>
         <Link href="https://www.linkedin.com/in/kasperi-jaakkola/" className="hover:underline hover:uppercase" rel="noopener noreferrer" target="_blank">
           Kasperi Jaakkola
         </Link>
