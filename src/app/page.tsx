107d9efa4f9fc72a64cb73a3987babf0c9cea20d
qkas <kassuvaa@gmail.com>
2025-02-15 13:19:44 +0200

    Added text box showcase and initial side-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import TopSection from "@/components/top-section";
 import RecentActivity from "@/components/recent-activity";
+import SideBar from "@/components/side-bar";
+import TextBox from "@/components/text-box";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 md:gap-20">
       <TopSection/>
-      <div>
-        <RecentActivity/>
-        {/*  */}
+      <div className="flex flex-wrap gap-5">
+        <div className="flex flex-col min-w-[250px] lg:min-w-[800px] flex-1 gap-5 lg:gap-10">
+          <TextBox/>
+          <RecentActivity/>
+        </div>
+        <SideBar/>
       </div>
       <div className="my-20 font-[family-name:var(--font-geist-mono)]">WEBSITE IN PROGRESS, MORE CONTENT COMING SOON</div>
     </div>
---
+++ src/components/side-bar.tsx
@@ -0,0 +1,8 @@
+export default function SideBar() {
+    return (
+        <div className="gap-5 sm:gap-10 border-2 rounded-lg p-5 font-[family-name:var(--font-geist-mono)]">
+            <span className="text-xl">Currently Online</span>
+            
+        </div>
+    )
+}
