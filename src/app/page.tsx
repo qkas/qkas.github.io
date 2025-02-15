ccf871b8d30abfcda3f774e81d734a541baf92fa
qkas <kassuvaa@gmail.com>
2025-02-16 01:33:33 +0200

    Added boilerplate skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import TopSection from "@/components/top-section";
 import RecentActivity from "@/components/recent-activity";
 import SideBar from "@/components/side-bar";
 import TextBox from "@/components/text-box";
+import Achievements from "@/components/achievements";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="flex flex-wrap gap-5">
         <div className="flex flex-col min-w-[250px] lg:min-w-[800px] flex-1 gap-5 lg:gap-10">
           <TextBox/>
+          <Achievements/>
           <RecentActivity/>
         </div>
         <SideBar/>
---
+++ src/components/achievements.tsx
@@ -0,0 +1,8 @@
+export default function Achievements() {
+	return (
+		<div id="skills" className="flex flex-col border-2 border-foreground divide-y-2 divide-foreground rounded-lg">
+			<h1 className="items-center text-center w-full px-5 py-2 font-bold text-base md:text-lg">Achievement Showcase</h1>
+			<div className="p-10 text-xl">skill section coming soon...</div>
+		</div>
+	)
+}
