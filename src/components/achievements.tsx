b7b87c8d46e0907d4b8a630d87b3229bc5179bf1
qkas <kassuvaa@gmail.com>
2025-12-21 13:09:43 +0200

    Update skills section

--- src/components/achievements.tsx
+++ src/components/achievements.tsx
@@ -15,7 +15,7 @@ export default function Achievements() {
 		<div id="skills" className="flex flex-col border-2 border-foreground divide-y-2 divide-foreground rounded-lg">
 			<h1 className="items-center text-center w-full px-5 py-2 font-bold text-base md:text-lg">Achievement Showcase</h1>
 			<div className="flex gap-2 md:gap-3 lg:gap-4 flex-wrap p-10 text-xl">
-				<Skill name="GitHub" />
+				<Skill name="Git" />
 				<Skill name="Node.js" />
 				<Skill name="JavaScript" />
 				<Skill name="TypeScript" />
@@ -27,10 +27,12 @@ export default function Achievements() {
 				<Skill name="TailwindCSS" />
 				<Skill name="React Native" />
 				<Skill name="C#" />
+				<Skill name="C++" />
 				<Skill name="Python" />
 				<Skill name="Java" />
 				<Skill name="Unity" />
 				<Skill name="Godot" />
+				<Skill name="Unreal" />
 				<Skill name="Blender" />
 			</div>
 		</div>
